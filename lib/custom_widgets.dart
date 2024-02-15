d91b784d2c0ac4bec7aa42d786ba66864883d713
admin <admin@Sekhars-MacBook-Pro.local>
2024-02-15 16:10:47 +0530

    updated

---
+++ lib/custom_widgets.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class custom_widgets {
+  
+  Widget CustomButton(
+      {required VoidCallback onPressed,
+      required String text,
+      MaterialStateProperty<Color?>? bgColor,
+      MaterialStateProperty<Color?>? textColor}) {
+    return TextButton(
+      onPressed: onPressed,
+      child: Text(text),
+      style: ButtonStyle(backgroundColor: bgColor),
+    );
+  }
+
+  Widget CustomTextField({TextEditingController? controller,text,required String title,bool? obscureText}) {
+    return  TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                  label: Text(title),
+                  border: InputBorder.none,
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 15, horizontal: 16),
+                ),
+                obscureText: obscureText??false ,
+                );
+                
+  }
+}
