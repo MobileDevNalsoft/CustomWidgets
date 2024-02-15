467e915f10c236716026fcd875da343832605f1b
admin <admin@Sekhars-MacBook-Pro.local>
2024-02-15 11:50:03 +0530

    Added custom button

--- lib/custom_button.dart
+++ lib/custom_button.dart
@@ -1,7 +1,25 @@
 library custom_button;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+
+class CustomButton extends StatefulWidget{
+
+  MaterialStateProperty<Color?>? bgColor;
+  MaterialStateProperty<Color?>? textColor;
+  String text;
+  double? rounded;
+  Function onPressed; 
+CustomButton({this.bgColor,this.textColor,required this.text,this.rounded,required this.onPressed});
+
+  @override
+  State<CustomButton> createState() => _CustomButtonState();
+}
+
+class _CustomButtonState extends State<CustomButton> {
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(onPressed: widget.onPressed(), child: Text(widget.text),style: ButtonStyle(backgroundColor: widget.bgColor),);
+  }
 }
