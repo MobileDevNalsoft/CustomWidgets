be7d664e1a4c5618feaab7ed68788942b0f613de
admin <admin@Sekhars-MacBook-Pro.local>
2024-02-15 16:32:39 +0530

    updated

---
+++ lib/src.dart
@@ -0,0 +1,5 @@
+library custom_widgets;
+
+export 'src/custom_button.dart';
+export 'src/custom_textfield.dart';
+export 'src/widgets.dart';
--- lib/src/src.dart
+++
@@ -1,5 +0,0 @@
-library custom_widgets;
-
-// export '../custom_button.dart';
-// export '../custom_textfield.dart';
-export '../customWidgets.dart';
