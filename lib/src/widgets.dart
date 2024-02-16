eb27640128e3e7099bfe94dd553a762013d89627
saisanjit <saisanjit.d@gmail.com>
2024-02-16 17:02:38 +0530

    updated

--- lib/src/widgets.dart
+++ lib/src/widgets.dart
@@ -14,17 +14,55 @@ class Widgets {
     );
   }
 
-  Widget CustomTextField({TextEditingController? controller,text,required String title,bool? obscureText}) {
-    return  TextField(
-                controller: controller,
-                decoration: InputDecoration(
-                  label: Text(title),
-                  border: InputBorder.none,
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 15, horizontal: 16),
-                ),
-                obscureText: obscureText??false ,
-                );
+  // Widget CustomTextField({TextEditingController? controller,text,required String title,bool? obscureText}) {
+  //   return  TextField(
+  //               controller: controller,
+  //               decoration: InputDecoration(
+  //                 label: Text(title),
+  //                 border: InputBorder.none,
+  //                 contentPadding:
+  //                     EdgeInsets.symmetric(vertical: 15, horizontal: 16),
+  //               ),
+  //               obscureText: obscureText??false ,
+  //               );
                 
+  // }
+
+  @override
+  Widget CustomTextField({String? label, String? hintText, prefixIcon, suffixIcon,TextEditingController? controller, bool? obscureText,String? obscuringCharacter}){
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: const [
+            BoxShadow(
+                color: Colors.grey,
+                offset: Offset(0, 5),
+                blurRadius: 5
+            ),
+            BoxShadow(
+              color: Colors.white,
+            ),
+          ]
+      ),
+      child: TextFormField(
+        controller: controller,
+        decoration: InputDecoration(
+          label:Text(label??""),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15)
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15)
+          ),
+          contentPadding: const EdgeInsets.symmetric(vertical: 15, horizontal: 16),
+          hintStyle: const TextStyle(color: Colors.black38),
+          hintText : hintText??'',
+          prefixIcon: prefixIcon??'',
+          suffixIcon: suffixIcon??'',
+        ),
+        obscuringCharacter: obscuringCharacter ?? '*',
+        obscureText: obscureText ?? false,
+      ),
+    );
   }
 }
